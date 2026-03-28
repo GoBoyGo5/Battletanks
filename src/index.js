5b0df5f910c09f516d14a92a146343b5c84db1e2
GoBoyGo <vityokvit@gmail.com>
2026-03-28 22:07:51 +0100

    0.0.0.dev-11

--- src/index.js
+++ src/index.js
@@ -53,7 +53,6 @@ tank select with team is if take 1 then cant take same
 import * as THREE from "https://unpkg.com/three/build/three.module.js";
 import { GLTFLoader } from "https://unpkg.com/three/examples/jsm/loaders/GLTFLoader.js";
 import { OrbitControls } from 'https://unpkg.com/three/examples/jsm/controls/OrbitControls.js';
-// import { PositionalAudioHelper } from 'three/addons/helpers/PositionalAudioHelper.js';
 
 const keys = {};
 const walls = [];
